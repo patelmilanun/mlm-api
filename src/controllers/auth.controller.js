103a55f3f85882200fa7d0df1e53080abb9f054f
Milan Patel <milan@pardypanda.com>
2021-03-30 00:41:36 +0530

    update register controller to add status, refered by, payment link for user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,34 @@
 const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { authService, userService, tokenService, emailService } = require('../services');
+const { generatePaymentLink } = require('../utils/payment');
+const { statusTypes } = require('../config/statuses');
+const { authService, userService, tokenService, emailService, statusService, orderService } = require('../services');
 
 const register = catchAsync(async (req, res) => {
-  const { name } = req.body;
-  const user = await userService.createUser({ ...req.body, profileImageHash: name });
+  const { name, referedBy } = req.body;
+  const userStatus = await statusService.getStatusByType(statusTypes.PAYMENT_PENDING);
+  if (!userStatus) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Status not found');
+  }
+  let refered;
+  if (referedBy === '') {
+    const user = await userService.getUserByEmail('admin@inspireonics.com');
+    refered = user.id;
+  } else {
+    const user = await userService.getUserById(referedBy);
+    if (!user) throw new ApiError(httpStatus.NOT_FOUND, 'The refereal id not found');
+  }
+  const user = await userService.createUser({
+    ...req.body,
+    profileImageHash: name,
+    status: userStatus.id,
+    referedBy: refered,
+  });
+  const order = await orderService.createOrder({ orderAmount: 100, createdBy: user.id });
   const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.CREATED).send({ user, tokens });
+  user.status = userStatus;
+  res.status(httpStatus.CREATED).send({ user, tokens, paymentLink: await generatePaymentLink(user, order) });
 });
 
 const login = catchAsync(async (req, res) => {
