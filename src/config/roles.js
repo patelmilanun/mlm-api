aa0f79b598f55212e048ae89f680132dd5c5209a
Milan Patel <patelmilanun@gmail.com>
2021-07-25 16:49:02 +0530

    add guest role, make guest role default for new user

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,8 +1,9 @@
-const roles = ['user', 'admin'];
+const roles = ['user', 'admin', 'guest'];
 
 const roleRights = new Map();
 roleRights.set(roles[0], ['getCourses']);
 roleRights.set(roles[1], ['getUsers', 'manageUsers', 'getCourses', 'manageCourses', 'getVideos', 'manageVideos']);
+roleRights.set(roles[2], []);
 
 module.exports = {
   roles,
