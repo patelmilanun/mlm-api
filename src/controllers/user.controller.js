0c26381c453aa1234cbeb5d944d8679e32563739
Milan Patel <patelmilanun@gmail.com>
2021-07-25 16:53:48 +0530

    add get me api to fetch current user information

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,6 +24,14 @@ const getUser = catchAsync(async (req, res) => {
   res.send(user);
 });
 
+const getMe = catchAsync(async (req, res) => {
+  const { user } = req;
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  res.send(user);
+});
+
 const updateUser = catchAsync(async (req, res) => {
   const user = await userService.updateUserById(req.params.userId, req.body);
   res.send(user);
@@ -38,6 +46,7 @@ module.exports = {
   createUser,
   getUsers,
   getUser,
+  getMe,
   updateUser,
   deleteUser,
 };
